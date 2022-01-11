002fc61f501f7fbc43c73eabdd194ac2f4445536
Fayaz Rafin <67512015+fayaz-rafin@users.noreply.github.com>
2022-01-11 06:37:00 +0000

    Added a .env file to protect bot token

--- main.py
+++ main.py
@@ -7,13 +7,14 @@ import asyncio
 from asyncio import gather
 
 #Prefix to call the bot
-client = commands.Bot(command_prefix = '!')
+client = commands.Bot(command_prefix='!')
+
 
 @client.event
 async def on_ready():
-  await client.change_presence(activity=discord.Activity(
+    await client.change_presence(activity=discord.Activity(
         type=discord.ActivityType.playing, name="Rock, Paper Scissors!"))
-  print('I have power!')
+    print('I have power!')
 
 
 #Users must type "!play {choice} to play"
@@ -25,34 +26,62 @@ async def play(ctx, message):
     if answer not in choices:
         await ctx.send("Please enter a valid choice")
     else:
-            if bot_choice == "rock" and answer == "paper":
-                embedVar = discord.Embed(title="You win!", description=f"**mello's choice:** {bot_choice}\n **your choice:** {answer}.", color=discord.Color.blurple())
-                await ctx.send(embed=embedVar)
+        if bot_choice == "rock" and answer == "paper":
+            embedVar = discord.Embed(
+                title="You win!",
+                description=
+                f"**mello's choice:** {bot_choice}\n **your choice:** {answer}.",
+                color=discord.Color.blurple())
+            await ctx.send(embed=embedVar)
 
-            elif bot_choice == "rock" and answer == "scissors":
-                embedVar = discord.Embed(title="mello wins!", description=f"**mello's choice:** {bot_choice}\n **your choice:** {answer}.", color=discord.Color.blurple())
-                await ctx.send(embed=embedVar)
+        elif bot_choice == "rock" and answer == "scissors":
+            embedVar = discord.Embed(
+                title="mello wins!",
+                description=
+                f"**mello's choice:** {bot_choice}\n **your choice:** {answer}.",
+                color=discord.Color.blurple())
+            await ctx.send(embed=embedVar)
 
-            elif bot_choice == "paper" and answer == "rock":
-                embedVar = discord.Embed(title="mello wins!", description=f"**mello's choice:** {bot_choice}\n **your choice:** {answer}.", color=discord.Color.blurple())
-                await ctx.send(embed=embedVar)
-                
-            elif bot_choice == "paper" and answer == "scissors":
-                embedVar = discord.Embed(title="You win!", description=f"**mello's choice:** {bot_choice}\n **your choice:** {answer}.", color=discord.Color.blurple())
-                await ctx.send(embed=embedVar)
+        elif bot_choice == "paper" and answer == "rock":
+            embedVar = discord.Embed(
+                title="mello wins!",
+                description=
+                f"**mello's choice:** {bot_choice}\n **your choice:** {answer}.",
+                color=discord.Color.blurple())
+            await ctx.send(embed=embedVar)
 
-            elif bot_choice == "scissors" and answer == "rock":
-                embedVar = discord.Embed(title="You win!", description=f"**mello's choice:** {bot_choice}\n **your choice:** {answer}.", color=discord.Color.blurple())
-                await ctx.send(embed=embedVar)
+        elif bot_choice == "paper" and answer == "scissors":
+            embedVar = discord.Embed(
+                title="You win!",
+                description=
+                f"**mello's choice:** {bot_choice}\n **your choice:** {answer}.",
+                color=discord.Color.blurple())
+            await ctx.send(embed=embedVar)
 
-            elif bot_choice == "scissors" and answer == "paper":
-                embedVar = discord.Embed(title="mello wins!", description=f"**mello's choice:** {bot_choice}\n **your choice:** {answer}.", color=discord.Color.blurple())
-                await ctx.send(embed=embedVar)
+        elif bot_choice == "scissors" and answer == "rock":
+            embedVar = discord.Embed(
+                title="You win!",
+                description=
+                f"**mello's choice:** {bot_choice}\n **your choice:** {answer}.",
+                color=discord.Color.blurple())
+            await ctx.send(embed=embedVar)
 
-            else:
-                embedVar = discord.Embed(title="It's a draw!", description=f"**mello's choice:** {bot_choice}\n **your choice:** {answer}.", color=discord.Color.blurple())
-                await ctx.send(embed=embedVar)
+        elif bot_choice == "scissors" and answer == "paper":
+            embedVar = discord.Embed(
+                title="mello wins!",
+                description=
+                f"**mello's choice:** {bot_choice}\n **your choice:** {answer}.",
+                color=discord.Color.blurple())
+            await ctx.send(embed=embedVar)
+
+        else:
+            embedVar = discord.Embed(
+                title="It's a draw!",
+                description=
+                f"**mello's choice:** {bot_choice}\n **your choice:** {answer}.",
+                color=discord.Color.blurple())
+            await ctx.send(embed=embedVar)
 
 
-client.run('OTMwMjg4NTM3NDU3Mjk1NDAy.Ydzstw.dR9V2GCbM6FAH_6YFe0bjqYhYJc')
 
+client.run(os.getenv('TOKEN'))
